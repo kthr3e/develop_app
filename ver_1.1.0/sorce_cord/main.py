ab3646bf9791294995566a29a9ac89ce4940e59f
kthr3e <kimiyuki001@gmail.com>
2020-10-24 23:54:16 +0900

    a

--- ver_1.1.0/sorce_cord/main.py
+++ ver_1.1.0/sorce_cord/main.py
@@ -1,16 +1,19 @@
 import pulp
 from get_nutrition_val_list import get_nutrition_val_list
-from flask import Flask
+from flask import Flask,make_response, jsonify
+import csv
+# flaskの設定
 app = Flask(__name__)
 
 @app.route('/')
+
 
 def main():
     #問題の定義　最小化か最大化か　
     # 今回はカロリーを最小化したい。
     problem = pulp.LpProblem(name ="1日の栄養素を満たすメニュー", sense = pulp.LpMinimize)
 
-    import csv
+
     McdonaldsDict = {}
     with open('nutrition_data/windows_macdonalds_nutrition.csv',encoding='cp932') as f:
         #print(f.read())
@@ -58,19 +61,19 @@ def main():
 
 
     one_da_nutrition_dict ={
-        "エネルギーkcal" : 2650.0,
-        "たんぱく質g" : 65.0 ,
-        "脂質g" : 73.6 ,
-        "炭水化物g" : 380.9 ,
-        "カルシウムmg" : 800.0 ,
-        "鉄mg" : 7.5 ,
-        "ビタミンAμg" : 850.0 ,
-        "ビタミンB1mg" : 1.4 ,
-        "ビタミンB2mg" : 1.6 ,
-        "ビタミンCmg" : 100.0 ,
-        "食物繊維g" : 21.0 ,
-        "食塩相当量g" : 7.5 ,
-        }
+        "エネルギー[kcal]" : 2650.0,
+        "たんぱく質[g]" : 65.0 ,
+        "脂質[g]" : 73.6 ,
+        "炭水化物[g]" : 380.9 ,
+        "カルシウム[mg]" : 800.0 ,
+        "鉄[mg]" : 7.5 ,
+        "ビタミンA[μg]" : 850.0 ,
+        "ビタミンB1[mg]" : 1.4 ,
+        "ビタミンB2[mg]" : 1.6 ,
+        "ビタミンC[mg]" : 100.0 ,
+        "食物繊維[g]" : 21.0 ,
+        "食塩相当量[g]" : 7.5 ,
+    }
 
     # 対象とする栄養素について、対象の商品リストごとの栄養価を、リスト形式で作成する
     eiyou_data = {}
@@ -83,26 +86,26 @@ def main():
     #lowBoundで0から∞まで
     #catで変数の種類指定
     xs = [pulp.LpVariable('{}'.format(x), cat = 'Integer', lowBound = 0) for x in target_menu_list]
-    #print("xs:",xs)
+    print("xs:",xs)
 
     # 目的関数：カロリーを最小化
     # lpdot:二つのリストのない席を求める。
-    problem += pulp.lpDot(eiyou_data["エネルギーkcal"],xs)
+    problem += pulp.lpDot(eiyou_data["エネルギー[kcal]"],xs)
 
     #制約条件：　一日に必要内容量をそれぞれ満たすこと
     # 条件カスタマイズ＆ON-OFFしやすいように、あえてループ外で起債。
     #食塩相当については、「以内」としている。解が存在スカは要注意
-    problem += pulp.lpDot(eiyou_data["たんぱく質g"], xs) >= one_da_nutrition_dict["たんぱく質g"]
-    problem += pulp.lpDot(eiyou_data["脂質g"], xs) >= one_da_nutrition_dict["脂質g"]
-    problem += pulp.lpDot(eiyou_data["炭水化物g"], xs) >= one_da_nutrition_dict["炭水化物g"]
-    problem += pulp.lpDot(eiyou_data["カルシウムmg"], xs) >= one_da_nutrition_dict["カルシウムmg"]
-    problem += pulp.lpDot(eiyou_data["鉄mg"], xs) >= one_da_nutrition_dict["鉄mg"]
-    problem += pulp.lpDot(eiyou_data["ビタミンAμg"], xs) >= one_da_nutrition_dict["ビタミンAμg"]
-    problem += pulp.lpDot(eiyou_data["ビタミンB1mg"], xs) >= one_da_nutrition_dict["ビタミンB1mg"]
-    problem += pulp.lpDot(eiyou_data["ビタミンB2mg"], xs) >= one_da_nutrition_dict["ビタミンB2mg"]
-    problem += pulp.lpDot(eiyou_data["ビタミンCmg"], xs) >= one_da_nutrition_dict["ビタミンCmg"]
-    problem += pulp.lpDot(eiyou_data["食物繊維g"], xs) >= one_da_nutrition_dict["食物繊維g"]
-    problem += pulp.lpDot(eiyou_data["食塩相当量g"], xs) <= one_da_nutrition_dict["食塩相当量g"]
+    problem += pulp.lpDot(eiyou_data["たんぱく質[g]"], xs) >= one_da_nutrition_dict["たんぱく質[g]"]
+    problem += pulp.lpDot(eiyou_data["脂質[g]"], xs) >= one_da_nutrition_dict["脂質[g]"]
+    problem += pulp.lpDot(eiyou_data["炭水化物[g]"], xs) >= one_da_nutrition_dict["炭水化物[g]"]
+    problem += pulp.lpDot(eiyou_data["カルシウム[mg]"], xs) >= one_da_nutrition_dict["カルシウム[mg]"]
+    problem += pulp.lpDot(eiyou_data["鉄[mg]"], xs) >= one_da_nutrition_dict["鉄[mg]"]
+    problem += pulp.lpDot(eiyou_data["ビタミンA[μg]"], xs) >= one_da_nutrition_dict["ビタミンA[μg]"]
+    problem += pulp.lpDot(eiyou_data["ビタミンB1[mg]"], xs) >= one_da_nutrition_dict["ビタミンB1[mg]"]
+    problem += pulp.lpDot(eiyou_data["ビタミンB2[mg]"], xs) >= one_da_nutrition_dict["ビタミンB2[mg]"]
+    problem += pulp.lpDot(eiyou_data["ビタミンC[mg]"], xs) >= one_da_nutrition_dict["ビタミンC[mg]"]
+    problem += pulp.lpDot(eiyou_data["食物繊維[g]"], xs) >= one_da_nutrition_dict["食物繊維[g]"]
+    problem += pulp.lpDot(eiyou_data["食塩相当量[g]"], xs) <= one_da_nutrition_dict["食塩相当量[g]"]
 
     #与えられた問題の内容を表示
     #print(problem)
@@ -117,13 +120,18 @@ def main():
     print("「一日に必要な栄養素を摂取するには」")
     for x in xs:
         print(str(x),":",str(int(x.value())),"個")
+
     print("\n")
     # それぞれの栄養素がいくらか
     print("栄養素の値")
     for key in one_da_nutrition_dict:
         print(key,":",str(one_da_nutrition_dict[key]),"に対し",str(round(pulp.lpDot(eiyou_data[key],xs).value())))
 
+    #return make_responce(jsonify(one_da_nutrition_dict))
+
 
 # python main.pyで実行されたときだけ動くようにする。
 if  __name__ == "__main__":		# importされると"__main__"は入らないので，実行かimportかを判断できる．
 	main()    # メイン関数を実行
+    #app.debug = True
+    #app.run()
