7cb2b2e2c0eb588cc8eb861b40d4b5be7cd5f775
kthr3e <kimiyuki001@gmail.com>
2020-11-04 00:39:42 +0900

    次に一日に必要な栄養素の取得のところで、栄養素の種類が一番少ないものを選びそのkeyだけ取得するものを作ればできそう

--- backend/ver_1.4.0/sorce_cord/index.py
+++ backend/ver_1.4.0/sorce_cord/index.py
@@ -12,7 +12,7 @@ def main():
     # 今回はカロリーを最小化したい。
     problem = pulp.LpProblem(name ="1日の栄養素を満たすメニュー", sense = pulp.LpMinimize)
 
-    data = {"menu":["macdonalds"]}
+    data = {"menu":["dennys"]}
     MenuDict = menu_dict(data)
     #print("wx: ",MenuDict)
 
@@ -62,7 +62,7 @@ def main():
     for key in one_da_nutrition_dict.keys():
         # keyに入っている栄養の名称を、データのdictのkeyにする。
         eiyou_data[key] = get_nutrition_val_list(MenuDict,target_menu_list,key)
-    #print("eiyou_data",eiyou_data)
+    print("eiyou_data",eiyou_data)
 
     # 変数の定義
     #LpVariableで自由辺巣を作成。値は-∞から∞まで
@@ -80,16 +80,13 @@ def main():
     #制約条件：　一日に必要内容量をそれぞれ満たすこと
     # 条件カスタマイズ＆ON-OFFしやすいように、あえてループ外で起債。
     #食塩相当については、「以内」としている。解が存在スカは要注意
-    problem += pulp.lpDot(eiyou_data["たんぱく質[g]"], xs) >= float(one_da_nutrition_dict["たんぱく質[g]"])
-    problem += pulp.lpDot(eiyou_data["脂質[g]"], xs) >= float(one_da_nutrition_dict["脂質[g]"])
-    problem += pulp.lpDot(eiyou_data["炭水化物[g]"], xs) >= float(one_da_nutrition_dict["炭水化物[g]"])
-    problem += pulp.lpDot(eiyou_data["カルシウム[mg]"], xs) >= float(one_da_nutrition_dict["カルシウム[mg]"])
-    problem += pulp.lpDot(eiyou_data["鉄[mg]"], xs) >= float(one_da_nutrition_dict["鉄[mg]"])
-    problem += pulp.lpDot(eiyou_data["ビタミンA[μg]"], xs) >= float(one_da_nutrition_dict["ビタミンA[μg]"])
-    problem += pulp.lpDot(eiyou_data["ビタミンB1[mg]"], xs) >= float(one_da_nutrition_dict["ビタミンB1[mg]"])
-    problem += pulp.lpDot(eiyou_data["ビタミンB2[mg]"], xs) >= float(one_da_nutrition_dict["ビタミンB2[mg]"])
-    problem += pulp.lpDot(eiyou_data["ビタミンC[mg]"], xs) >= float(one_da_nutrition_dict["ビタミンC[mg]"])
-    problem += pulp.lpDot(eiyou_data["食物繊維[g]"], xs) >= float(one_da_nutrition_dict["食物繊維[g]"])
+    # 栄養素を存在するかどうかをkeyがあるかどうかの判定にする
+    # この場合だとメニューを組み合わせたときにエラーが起こる
+    for key in eiyou_data.keys():
+        if key == "食塩相当量[g]":
+            continue
+        problem += pulp.lpDot(eiyou_data[key], xs) >= float(one_da_nutrition_dict[key])
+
     problem += pulp.lpDot(eiyou_data["食塩相当量[g]"], xs) <= float(one_da_nutrition_dict["食塩相当量[g]"])
 
     #与えられた問題の内容を表示
@@ -106,7 +103,7 @@ def main():
         print("「一日に必要な栄養素を摂取するには」")
         for x in xs:
             #print("x.value:",x.value)
-            print(str(x),":",x.value(),"個")
+            print(str(x),":",str(int(x.value())),"個")
 
         print("\n")
         # それぞれの栄養素がいくらか
