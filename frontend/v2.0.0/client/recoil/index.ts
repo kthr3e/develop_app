690443ad535c6c75928c6bf71bc85f8e9c12fe70
NozomuTsuruta <tuno.0806@icloud.com>
2020-11-26 23:51:16 +0900

    chartの追加

--- frontend/v2.0.0/client/recoil/index.ts
+++ frontend/v2.0.0/client/recoil/index.ts
@@ -9,13 +9,6 @@ export const result_value = atom<ResultType>({
   default: {} as ResultType,
 });
 
-export type ToastType = [string, ("error" | "info" | "success" | "warning")?];
-
-export const toast_value = atom<ToastType>({
-  key: "toast_value",
-  default: [""],
-});
-
 export const menu_value = atom<string[]>({
   key: "menu_value",
   default: [],
